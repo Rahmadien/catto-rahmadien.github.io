856924f446e90d21ed48b480d790c7c510c4ccc4
rhmdnakbrzsyflhq@gmail.com <rhmdnakbrzsyflhq@gmail.com>
2022-06-26 14:25:23 +0700

    Tugas JS API FGA

---
+++ src/Pages/About.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+// import axios from "axios";
+import "./About.css"
+
+function About() {
+
+
+   return (
+      <div className="card" >
+         <p> About catto: </p>
+         <p> Catto is an app for cat lovers</p>
+         <p> Made using React.js </p>
+         <p> Using API from thecatapi.com </p>
+         <p> Made by: Rahmadien Akbariza</p>
+
+      </div>
+   );
+}
+
+export default About;
